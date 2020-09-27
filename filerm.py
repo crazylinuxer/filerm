a8ad1be805f0cd74ff04f3ba2db3742ffda24664
crazylinuxer <kvirikroma@gmail.com>
2020-09-27 16:50:01 +0300

    tried to fix parameters recognition

--- filerm.py
+++ filerm.py
@@ -10,7 +10,7 @@ def print_help():
 
 
 def exit_on_syntax_error():
-    print("Invalid command line parameters detected!", file=sys.stderr)
+    print("Invalid command line parameters detected!", file=sys.stderr, end="\n\n")
     print_help()
     exit(1)
 
@@ -23,23 +23,19 @@ parameters = {
     "directory": os.path.curdir
 }
 
-
 files_to_delete = []
 
 
+skip = False
+
 for i in range(1, len(sys.argv)):
+    if skip:
+        skip = False
+        continue
     if sys.argv[i][0] == '-':
         if len(sys.argv[i]) == 1:
             if i >= len(sys.argv) - 1:
                 parameters["directory"] = sys.argv[i]
-            elif i > 1 and (
-                sys.argv[i - 1] == "--pattern" or (
-                    sys.argv[i - 1][0] == '-' and
-                    sys.argv[i - 1][1] != '-' and
-                    'p' in sys.argv[i - 1]
-                )
-            ):
-                pass
             else:
                 exit_on_syntax_error()
         elif sys.argv[i][1] == '-':
@@ -54,35 +50,51 @@ for i in range(1, len(sys.argv)):
                     parameters["directory"] = sys.argv[i]
                 elif i + 1 <= len(sys.argv):
                     parameters["pattern"] = sys.argv[i + 1]
+                    skip = True
                 else:
                     exit_on_syntax_error()
         else:
             for param in range(1, len(sys.argv[i])):
-                if param == "h":
+                if sys.argv[i][param] == "h":
                     parameters["help"] = True
-                elif param == "r":
+                elif sys.argv[i][param] == "r":
                     parameters["recursively"] = True
-                elif param == "i":
+                elif sys.argv[i][param] == "i":
                     parameters["hidden"] = True
-                elif param == "p":
+                elif sys.argv[i][param] == "p":
                     if i + 1 <= len(sys.argv):
                         parameters["pattern"] = sys.argv[i + 1]
+                        skip = True
                     else:
                         exit_on_syntax_error()
+                else:
+                    exit_on_syntax_error()
+    elif i >= len(sys.argv) - 1:
+        parameters["directory"] = sys.argv[i]
+    else:
+        exit_on_syntax_error()
+
 
 if parameters["help"]:
     print_help()
     exit(0)
 
 
+def append_if_not_hidden(directory: str, file: str):
+    if file[0] == '.':
+        if parameters["hidden"]:
+            files_to_delete.append(os.path.join(directory, file))
+    else:
+        files_to_delete.append(os.path.join(directory, file))
+
+
 def append_files(directory: str, files: Iterable[str]):
     for file in files:
-        if parameters["pattern"] and re.fullmatch(parameters["pattern"], file):
-            if file[0] == '.':
-                if parameters["hidden"]:
-                    files_to_delete.append(os.path.join(directory, file))
-            else:
-                files_to_delete.append(os.path.join(directory, file))
+        if parameters["pattern"]:
+            if re.fullmatch(parameters["pattern"], file):
+                append_if_not_hidden(directory, file)
+        else:
+            append_if_not_hidden(directory, file)
 
 
 if parameters["recursively"]:
@@ -111,6 +123,7 @@ if files_to_delete:
                 print(f"Failed to remove {file_name} (permission denied)", file=sys.stderr)
             except FileNotFoundError:
                 print(f"Failed to remove {file_name} (file does not exist)", file=sys.stderr)
+        print("Done!")
     else:
         print("Cancelled")
 
